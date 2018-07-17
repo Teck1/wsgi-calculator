f5ad0d9c6517f9c9996fd76e61e4dbd2ba38bc84
Teck1 <Teck1@users.noreply.github.com>
2018-07-17 07:30:23 -0700

    Add dispatch table and function. Add functions for math operations.

--- calculator.py
+++ calculator.py
@@ -40,45 +40,127 @@ To submit your homework:
 
 
 """
+#import traceback
 
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    try:
+        total = sum(list(map(int, args)))
+    except ValueError:
+        return "Please enter numeric values for the calculation."
+    return str(total)
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+def divide(*args):
+    """ Returns a STRING with the first arg in the list divided by the second arg"""
+    try:
+        total = int(args[0]) / int(args[1])
+    except ValueError:
+        return "Please enter numeric values for the calculation."
+    except ZeroDivisionError:
+        return "Nice try. No dividing by zero. You know the rules."
+    return str(total)
 
-    return sum
+def multiply(*args):
+    """ Returns a STRING with the first arg in the list multiplied by the second arg"""
+    try:
+        total = int(args[0]) * int(args[1])
+    except ValueError:
+        return "Please enter numeric values for the calculation."
+    return str(total)
 
-# TODO: Add functions for handling more arithmetic operations.
+def subtract(*args):
+    try:
+        total = int(args[0]) - sum(list(map(int, args[1:])))
+    except ValueError:
+        return "Please enter numeric values for the calculation."
+    return str(total)
+
+
+def instruct(*args):
+    page = """
+    <h1>Simple Calculator</h1><br>
+    <p><b>Usage:</b><br><br>
+        ADD: takes a list of operands <br>
+            &emsp;http://host:port/add/&ltop1&gt/&ltop2&gt/.../&ltopn&gt <br>
+            &emsp;ex: <br>
+            &emsp;http://host:8080/add/5/4/3/2/1 <br>
+            &emsp;returns 15 <br><br>
+        SUBTRACT: takes a list of operands <br>
+            &emsp;http://host:port/subtract/&ltop1&gt/&ltop2&gt/.../&ltopn&gt <br>
+            &emsp;ex: <br>
+            &emsp;http://host:8080/subtract/10/5/4/4 <br>
+            &emsp;returns -3 <br><br>
+        MULTIPLY: takes only 2 operands <br>
+            &emsp;http://host:port/multipy/&ltop1&gt/&ltop2&gt <br>
+            &emsp;ex: <br>
+            &emsp;http://host:8080/multiply/10/2 <br>
+            &emsp;returns 20 <br><br>
+        DIVIDE: takes only 2 operands <br>
+            &emsp;http://host:port/divide/&ltop1&gt/&ltop2&gt <br>
+            &emsp;ex: <br>
+            &emsp;http://host:8080/divide/10/5 <br>
+            &emsp;returns 2.0 <br><br>
+    </p>
+    """
+    return page
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
+    funcs = {
+        '': instruct,
+        'add': add,
+        'subtract': subtract,
+        'divide': divide,
+        'multiply': multiply,
+    }
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
+    if not len(args) >= 2:
+        func = funcs['']
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+
+    headers = [('Content-type', 'text/html')]
+    body = ""
+    status = ""
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+        # print(traceback.format_exc())
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1> Internal Server Error</h1>"
+        # print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
